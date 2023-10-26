493446fc9242ec897a4519e82d9e11aef4adf227
mnvivek6 <vivekmn04@gmail.com>
2023-10-26 12:29:02 +0530

    changeddd

--- src/interface/controller/admin/getTutors.ts
+++ src/interface/controller/admin/getTutors.ts
@@ -58,7 +58,7 @@ export const unverifiedTutors = async(req:Request, res:Response)=>{
         console.log(response,'response form backend');
         
         if (!response) {
-            throw new AppError("something went wronggg ",400)
+            throw new AppError("something went wrongggg ",400)
         }
       res.status(200).json(response)
     } catch (error:any) {
