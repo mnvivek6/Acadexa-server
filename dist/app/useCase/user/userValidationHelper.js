5b001d34d13ccc5b5a33451c0ae8303a0ed0f842
mnvivek6 <vivekmn04@gmail.com>
2024-09-13 09:34:46 +0530

    bcrypt js updation

--- dist/app/useCase/user/userValidationHelper.js
+++ dist/app/useCase/user/userValidationHelper.js
@@ -13,18 +13,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createToken = exports.passwordCompare = exports.passwordHashing = void 0;
-const bcrypt_1 = __importDefault(require("bcrypt"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
 dotenv_1.default.config({ path: path_1.default.resolve(__dirname, '../.env') });
 const passwordHashing = (password) => __awaiter(void 0, void 0, void 0, function* () {
-    const hashedPassword = yield bcrypt_1.default.hash(password, 10);
+    const hashedPassword = yield bcryptjs_1.default.hash(password, 10);
     return hashedPassword;
 });
 exports.passwordHashing = passwordHashing;
 const passwordCompare = (plainTextPassword, hashedPassword) => __awaiter(void 0, void 0, void 0, function* () {
-    const password = yield bcrypt_1.default.compare(plainTextPassword, hashedPassword);
+    const password = yield bcryptjs_1.default.compare(plainTextPassword, hashedPassword);
     return password;
 });
 exports.passwordCompare = passwordCompare;
