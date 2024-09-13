755bbaa2176f03fb63f2beeda81d0fcc00ccd210
mnvivek6 <vivekmn04@gmail.com>
2024-09-13 09:17:01 +0530

    bcrypt js updation

--- dist/app.js
+++ dist/app.js
@@ -21,7 +21,7 @@ app.use((0, morgan_1.default)('dev'));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 dotenv_1.default.config();
-(0, dbConfig_1.default)("mongodb+srv://mvivekmn:1234@cluster0.kb2qbes.mongodb.net/acadexa?retryWrites=true&w=majority");
+(0, dbConfig_1.default)(process.env.MONGODB_CONNECTION_URL || '');
 //setup routes
 app.use('/', user_1.default);
 app.use('/admin', admin_1.default);
@@ -38,7 +38,7 @@ const errorHandler = (error, req, res, next) => {
     });
 };
 app.use(errorHandler);
-const PORT = Number(process.env.PORT) || 4000;
+const PORT = Number(process.env.PORT) || 3000;
 const server = app.listen(4000, () => console.log(`server is running ${PORT}`));
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
