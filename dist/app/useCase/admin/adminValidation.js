c94cff6fc7a5802c62eebd73e052f0342b92b734
mnvivek6 <vivekmn04@gmail.com>
2024-09-13 09:49:06 +0530

    bcrypt js updation

--- dist/app/useCase/admin/adminValidation.js
+++ dist/app/useCase/admin/adminValidation.js
@@ -15,16 +15,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createToken = exports.passwordCompare = exports.passwordHashing = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
-const bcrypt_1 = __importDefault(require("bcrypt"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 dotenv_1.default.config({ path: path_1.default.resolve(__dirname, '../.env') });
 const passwordHashing = (password) => __awaiter(void 0, void 0, void 0, function* () {
-    const hashedPassword = yield bcrypt_1.default.hash(password, 10);
+    const hashedPassword = yield bcryptjs_1.default.hash(password, 10);
     return hashedPassword;
 });
 exports.passwordHashing = passwordHashing;
 const passwordCompare = (plainTextPassword, hashedPassword) => __awaiter(void 0, void 0, void 0, function* () {
-    const passowrd = yield bcrypt_1.default.compare(plainTextPassword, hashedPassword);
+    const passowrd = yield bcryptjs_1.default.compare(plainTextPassword, hashedPassword);
     return passowrd;
 });
 exports.passwordCompare = passwordCompare;
